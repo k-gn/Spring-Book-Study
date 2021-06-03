1dfdd39fd42a0f07308c8b157b638b71ba800481
k-gn <rlarbska97@gmail.com>
2021-06-03 16:00:54 +0900

    영화 평점 사이트 진행

---
+++ mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTest.java
@@ -0,0 +1,39 @@
+package org.zerock.mreview.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mreview.entity.Member;
+import org.zerock.mreview.entity.Movie;
+import org.zerock.mreview.entity.Review;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    public void insertReviews() {
+
+        IntStream.rangeClosed(1, 200).forEach(i -> {
+            Long mno = (long)(Math.random() * 100) + 1;
+
+            Long mid = (long)(Math.random() * 100) + 1;
+
+            Member member = Member.builder().mid(mid).build();
+
+            Review movieReview = Review.builder()
+                    .member(member)
+                    .movie(Movie.builder().mno(mno).build())
+                    .grade((int)(Math.random() * 5) + 1)
+                    .text("this movie is ..." + i)
+                    .build();
+            reviewRepository.save(movieReview);
+        });
+    }
+}
