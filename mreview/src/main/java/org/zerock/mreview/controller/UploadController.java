e76c4387861308f4a3fd8d961983f0352c24916f
k-gn <rlarbska97@gmail.com>
2021-06-05 09:46:54 +0900

    no message

--- mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,9 +1,13 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +15,10 @@ import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -28,7 +36,7 @@ public class UploadController {
 
     @PostMapping("/uploadAjax")
     public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
-
+        log.info("start uploadAjax");
         List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for(MultipartFile uploadFile : uploadFiles) {
@@ -55,11 +63,19 @@ public class UploadController {
             try {
 //                uploadFile.transferTo(savePath);
                 uploadFile.transferTo(saveFile);
+
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        log.info("end uploadAjax");
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
@@ -75,7 +91,55 @@ public class UploadController {
         if(!uploadPathFolder.exists()) {
             uploadPathFolder.mkdirs();
         }
-
         return folderPath;
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName : " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file : " + file);
+
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+        }catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(String fileName) {
+
+        String srcFileName = null;
+
+        try {
+            srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            File file = new File(uploadPath + File.separator + srcFileName);
+            boolean result = file.delete();
+
+            File thumbnail = new File(file.getParent(), "s_" + file.getName());
+            log.info("file.getParent() : " + file.getParent());
+            log.info("file.getName() : " + file.getName());
+
+            result = thumbnail.delete();
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
+++ mreview/src/main/java/org/zerock/mreview/dto/UploadResultDTO.java
@@ -2,6 +2,7 @@ package org.zerock.mreview.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
@@ -9,15 +10,28 @@ import java.net.URLEncoder;
 
 @Data
 @AllArgsConstructor
+@Log4j2
 public class UploadResultDTO implements Serializable {
 
     private String fileName;
     private String uuid;
     private String folderPath;
 
+    // getXXX 메소드는 브라우저로 전송되면 자동으로 사용 가능
     public String getImageURL() {
+        log.info("getImageURL");
         try {
             return URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getThumbnailURL() {
+        log.info("getThumbnailURL");
+        try {
+            return URLEncoder.encode(folderPath + "/s_" + uuid + "_" + fileName, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
